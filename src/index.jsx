bdef82fddc5c8d92304213719fd0e2c425a7ee08
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-01 08:32:37 +0530

    added browserrouter in root file of react

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // importing App
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
