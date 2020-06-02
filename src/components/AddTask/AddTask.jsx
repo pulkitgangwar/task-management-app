7164b3801b3e394568aca3a270fde85c93340191
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-02 16:18:46 +0530

    addtask component and addtaskform component completed (datepicker left)

---
+++ src/components/AddTask/AddTask.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// importing Components
+import AddTaskForm from "./AddTaskForm";
+
+const AddTask = () => {
+  return (
+    <div className="addtask">
+      <AddTaskForm />
+    </div>
+  );
+};
+
+export default AddTask;
--- src/components/AuthApp.jsx
+++ src/components/AuthApp.jsx
@@ -3,11 +3,13 @@ import { Switch, Route } from "react-router-dom";
 
 // importing Components
 import Dashboard from "./Dashboard/Dashboard";
+import AddTask from "./AddTask/AddTask";
 
 const AuthApp = () => {
   return (
     <Switch>
       <Route exact path="/" component={Dashboard} />
+      <Route exact path="/addtask" component={AddTask} />
       <Route exact render={() => <h1>Page Not Found</h1>} />
     </Switch>
   );
