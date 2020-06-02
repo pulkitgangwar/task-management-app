e23eee72acee01ab004501d0abee1cf6d913ac4a
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-02 07:44:33 +0530

    login register dashboard folders main file index.jsx has been changed to there folder name

--- src/components/AuthApp.jsx
+++ src/components/AuthApp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 // importing Components
-import Dashboard from "./Dashboard/index";
+import Dashboard from "./Dashboard/Dashboard";
 
 const AuthApp = () => {
   return (
--- src/components/UnAuthApp.jsx
+++ src/components/UnAuthApp.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 // importing Components
-import Login from "./Login/index";
-import Register from "./Register/index";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
 
 const UnAuthApp = () => {
   return (
