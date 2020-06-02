b6b546b587bf6e4e770656f29c2400ddb5d628bd
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-02 12:02:38 +0530

    filternav height adjustment

--- src/components/Dashboard/Navigation.jsx
+++ src/components/Dashboard/Navigation.jsx
@@ -15,7 +15,7 @@ const Navigation = ({ isNavOpen, setIsNavOpen }) => {
         <h2 className="navigation__heading__logo">Logo</h2>
       </div>
       <div className="navigation__for-user">
-        <h3 className="paragraph-primary  navigation__for-user__logout">
+        <h3 className="paragraph-secondary  navigation__for-user__logout">
           Logout
         </h3>
         <div className="navigation__for-user__hamburger"></div>
