052e0869117908b8dcb2a816c715d7507dcd2b9d
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-02 09:12:29 +0530

    taskdashboard component completed

--- src/components/Dashboard/TaskDashboard.jsx
+++ src/components/Dashboard/TaskDashboard.jsx
@@ -9,6 +9,14 @@ const TaskDashboard = ({ isNavOpen }) => {
       <div className="taskdashboard__sidenavigation">
         <SideNavigation isNavOpen={isNavOpen} />
       </div>
+      <div className="taskdashboard__header">
+        <h3 className="heading-tertiary heading-tertiary--small taskdashboard__task-count">
+          Total Tasks : 2
+        </h3>
+        <div className="taskdashboard__btn--wrapper">
+          <button className="btn taskdashboard__btn">Add Task</button>
+        </div>
+      </div>
     </div>
   );
 };
