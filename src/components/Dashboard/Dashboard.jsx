8f1ba465223695e5a2eb6fd78be3dcfffba46481
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-02 08:36:52 +0530

    togglenavigation function created to avoid both navigations to open at same time

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,15 +4,42 @@ import React, { useState } from "react";
 import Navigation from "./Navigation";
 import FilterNav from "./FilterNav";
 import TaskDashboard from "./TaskDashboard";
+import UserNavigation from "./UserNavigation";
 
 const Dashboard = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isUserNavOpen, setIsUserNavOpen] = useState(false);
+
+  const toggleNavigation = (navType) => {
+    if (navType === "usernav") {
+      if (isNavOpen) {
+        setIsNavOpen(false);
+        setIsUserNavOpen(!isUserNavOpen);
+        return;
+      }
+      setIsUserNavOpen(!isUserNavOpen);
+      return;
+    }
+    if (navType === "sidenav") {
+      if (isUserNavOpen) {
+        setIsUserNavOpen(false);
+        setIsNavOpen(!isNavOpen);
+        return;
+      }
+      setIsNavOpen(!isNavOpen);
+      return;
+    }
+  };
 
   return (
     <div className="dashboard">
-      <Navigation setIsNavOpen={setIsNavOpen} isNavOpen={isNavOpen} />
+      <Navigation toggleNavigation={toggleNavigation} />
       <FilterNav />
       <TaskDashboard isNavOpen={isNavOpen} />
+      <UserNavigation
+        setIsUserNavOpen={setIsUserNavOpen}
+        isUserNavOpen={isUserNavOpen}
+      />
     </div>
   );
 };
