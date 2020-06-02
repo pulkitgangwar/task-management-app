e26b512a8613f1a5ba61b59f80806ecbe7ff975c
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-02 10:28:27 +0530

    user navigation component removed, toggle navigation function removed

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,42 +4,15 @@ import React, { useState } from "react";
 import Navigation from "./Navigation";
 import FilterNav from "./FilterNav";
 import TaskDashboard from "./TaskDashboard";
-import UserNavigation from "./UserNavigation";
 
 const Dashboard = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [isUserNavOpen, setIsUserNavOpen] = useState(false);
-
-  const toggleNavigation = (navType) => {
-    if (navType === "usernav") {
-      if (isNavOpen) {
-        setIsNavOpen(false);
-        setIsUserNavOpen(!isUserNavOpen);
-        return;
-      }
-      setIsUserNavOpen(!isUserNavOpen);
-      return;
-    }
-    if (navType === "sidenav") {
-      if (isUserNavOpen) {
-        setIsUserNavOpen(false);
-        setIsNavOpen(!isNavOpen);
-        return;
-      }
-      setIsNavOpen(!isNavOpen);
-      return;
-    }
-  };
 
   return (
     <div className="dashboard">
-      <Navigation toggleNavigation={toggleNavigation} />
+      <Navigation isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
       <FilterNav />
       <TaskDashboard isNavOpen={isNavOpen} />
-      <UserNavigation
-        setIsUserNavOpen={setIsUserNavOpen}
-        isUserNavOpen={isUserNavOpen}
-      />
     </div>
   );
 };
