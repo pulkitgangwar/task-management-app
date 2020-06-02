0275f20dc0f1dbb5af4b40161c88d3218dc5de27
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-02 13:19:16 +0530

    sidenavigation structure completed

--- src/components/Dashboard/SideNavigation.jsx
+++ src/components/Dashboard/SideNavigation.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoIosArrowDown } from "react-icons/io";
 
 const SideNavigation = ({ isNavOpen }) => {
   return (
@@ -7,9 +8,66 @@ const SideNavigation = ({ isNavOpen }) => {
         isNavOpen ? "" : "sidenavigation__navigation-off"
       }`}
     >
-      <div className={`sidenavigation__content ${
-        isNavOpen ? "" : "sidenavigation__content__navigation-off"
-      }`}>asldjf alskdjf alksdfj</div>
+      <div
+        className={`sidenavigation__content ${
+          isNavOpen ? "" : "sidenavigation__content__navigation-off"
+        }`}
+      >
+      sjfsdfj 
+      asdjf 
+
+      asdfkj
+        {/* <ul className="sidenavigation__ul">
+          <li className="sidenavigation__li sidenavigation__drop ">
+            <div className="sidenavigation__drop__title">
+              <p className="paragraph-primary">Status Toggle</p>
+              <IoIosArrowDown />
+            </div>
+            <ul className="sidenavigation__drop__ul">
+              <li className="sidenavigation__drop__li">
+                <p className="paragraph-secondary">Pending</p>
+              </li>
+              <li className="sidenavigation__drop__li">
+                <p className="paragraph-secondary">In Progress</p>
+              </li>
+              <li className="sidenavigation__drop__li  sidenavigation__drop__li--active">
+                <p className="paragraph-secondary">Done</p>
+              </li>
+            </ul>
+          </li>
+
+        
+          <li className="sidenavigation__li sidenavigation__drop ">
+            <div className="sidenavigation__drop__title">
+              <p className="paragraph-primary">Labels</p>
+              <IoIosArrowDown />
+            </div>
+            <ul className="sidenavigation__drop__ul sidenavigation__drop__ul--active">
+              <li className="sidenavigation__drop__li">
+                <p className="paragraph-secondary">Personal</p>
+              </li>
+              <li className="sidenavigation__drop__li">
+                <p className="paragraph-secondary">Work</p>
+              </li>
+              <li className="sidenavigation__drop__li  sidenavigation__drop__li--active">
+                <p className="paragraph-secondary">Shopping</p>
+              </li>
+              <li className="sidenavigation__drop__li  sidenavigation__drop__li--active">
+                <p className="paragraph-secondary">Travel</p>
+              </li>
+              <li className="sidenavigation__drop__li  sidenavigation__drop__li--active">
+                <p className="paragraph-secondary">Home</p>
+              </li>
+              <li className="sidenavigation__drop__li">
+                <p className="paragraph-secondary">Office</p>
+              </li>
+              <li className="sidenavigation__drop__li">
+                <p className="paragraph-secondary">Other</p>
+              </li>
+            </ul>
+          </li>
+        </ul> */}
+      </div>
     </div>
   );
 };
