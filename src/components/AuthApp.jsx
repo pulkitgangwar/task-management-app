ee97b0dc09d443ce8825303b1f638171cd83a831
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-03 17:07:20 +0530

    added a dynamic singletaskroute to authapp component

--- src/components/AuthApp.jsx
+++ src/components/AuthApp.jsx
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
 import AddTask from "./AddTask/AddTask";
 import EditTask from "./EditTask/EditTask";
+import SingleTask from "./SingleTask/SingleTask";
 
 const AuthApp = () => {
   return (
@@ -12,6 +13,7 @@ const AuthApp = () => {
       <Route exact path="/" component={Dashboard} />
       <Route exact path="/addtask" component={AddTask} />
       <Route exact path="/edittask" component={EditTask} />
+      <Route exact path="/tasks/:id" component={SingleTask} />
       <Route exact render={() => <h1>Page Not Found</h1>} />
     </Switch>
   );
