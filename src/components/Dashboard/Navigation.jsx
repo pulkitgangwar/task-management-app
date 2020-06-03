e3e76477c96aa56cae27402a0a55b377bdc2c88d
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-03 13:51:48 +0530

    logout function implemented in navigation components

--- src/components/Dashboard/Navigation.jsx
+++ src/components/Dashboard/Navigation.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 // import {GrClose} from "react-icons/gr";
 
+// importing consumer
+import { Auth } from "../../context/Auth.context";
+
 const Navigation = ({ isNavOpen, setIsNavOpen }) => {
+  const { logout } = useContext(Auth);
   return (
     <nav className="navigation">
       <div className="navigation__heading">
@@ -15,7 +19,10 @@ const Navigation = ({ isNavOpen, setIsNavOpen }) => {
         <h2 className="navigation__heading__logo">Logo</h2>
       </div>
       <div className="navigation__for-user">
-        <h3 className="paragraph-secondary  navigation__for-user__logout">
+        <h3
+          onClick={logout}
+          className="paragraph-secondary  navigation__for-user__logout"
+        >
           Logout
         </h3>
         <div className="navigation__for-user__hamburger"></div>
