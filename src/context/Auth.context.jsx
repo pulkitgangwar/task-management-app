73897b9dc38a25f71b0f7492e2fd41dd51fe245b
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-03 13:24:33 +0530

    register function in authcontext component completed

--- src/context/Auth.context.jsx
+++ src/context/Auth.context.jsx
@@ -1,14 +1,36 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+// importing AuthClient
+import AuthClient from "../Utils/AuthClient";
 
 export const Auth = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(
-    localStorage.getItem("access") ? true : false
-  );
+  const [isLoggedIn] = useState(localStorage.getItem("access") ? true : false);
 
-  const register = (name, email, password) => {
-    console.log(name, email, password);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { push } = useHistory();
+
+  const register = async (name, email, password) => {
+    try {
+      console.log(name, email, password);
+      const authClient = new AuthClient();
+      setLoading(true);
+      await authClient.register(name, email, password);
+      setLoading(false);
+      setError(null);
+      console.log("near push");
+      push("/");
+    } catch (err) {
+      console.log("catch block ran");
+      console.log(err);
+      setError(err);
+      setLoading(false);
+      console.log("catch ran");
+      console.log(err);
+    }
   };
 
   const login = (email, password) => {
@@ -20,7 +42,9 @@ const AuthProvider = ({ children }) => {
   };
 
   return (
-    <Auth.Provider value={{ isLoggedIn, login, logout, register }}>
+    <Auth.Provider
+      value={{ isLoggedIn, login, logout, register, loading, error }}
+    >
       {children}
     </Auth.Provider>
   );
