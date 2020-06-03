f3035eb9f2655219db86ac2c5e657d0b6c42308d
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-03 15:24:46 +0530

    taskclient gettask function completed

---
+++ src/Utils/TaskClient.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+class TaskClient {
+  constructor() {
+    this.instance = axios.create({
+      baseURL: process.env.REACT_APP_API_URL,
+    });
+
+    this.instance.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${JSON.parse(localStorage.getItem("access"))}`;
+  }
+
+  //   read
+  getTasks() {
+    return new Promise((resolve, reject) => {
+      this.instance
+        .get("/tasks")
+        .then((data) => {
+          resolve(data.data);
+        })
+        .catch((err) => {
+          console.log(err.response);
+          if (Array.isArray(err.response.data.message)) {
+            reject(err.response.data.message[0]);
+            return;
+          } else if (err.response.data.message) {
+            reject(err.response.data.message);
+            return;
+          }
+        });
+    });
+  }
+}
+
+export default TaskClient;
