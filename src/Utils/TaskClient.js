dd6058fe86d9d3ac534c8740a6343dfbc4df90c4
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-03 18:44:03 +0530

    addtask method completed in taskclient

--- src/Utils/TaskClient.js
+++ src/Utils/TaskClient.js
@@ -52,6 +52,40 @@ class TaskClient {
         });
     });
   }
+
+  // create
+
+  addTask({ title, description, status, label, priority, due_date }) {
+    return new Promise((resolve, reject) => {
+      if (!title || !status || !label || !priority) {
+        reject("Please fill out all the important fields");
+        return;
+      }
+
+      this.instance
+        .post(`/tasks`, {
+          title,
+          description,
+          status,
+          label,
+          priority,
+          due_date,
+        })
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((err) => {
+          console.log(err.response);
+          if (Array.isArray(err.response.data.message)) {
+            reject(err.response.data.message[0]);
+            return;
+          } else if (err.response.data.message) {
+            reject(err.response.data.message);
+            return;
+          }
+        });
+    });
+  }
 }
 
 export const taskClient = new TaskClient();
