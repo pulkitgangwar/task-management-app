ff870d9d22db39a2b56ec2a67531f67491c92ee8
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-06-05 11:18:06 +0530

    priority added to task component

--- src/components/Dashboard/Task.jsx
+++ src/components/Dashboard/Task.jsx
@@ -4,7 +4,7 @@ import { formatDistance } from "date-fns";
 
 const Task = ({ task }) => {
   const { push } = useHistory();
-  const { id, title, status, due_date, label } = task;
+  const { id, title, status, due_date, label, priority } = task;
 
   return (
     <div className="task" id={id} onClick={() => push(`/tasks/${id}`)}>
@@ -12,9 +12,7 @@ const Task = ({ task }) => {
         <h3 className="heading-tertiary task__title-and-priority__title">
           {title}
         </h3>
-        <div className="task__title-and-priority__priority">
-          Priority for stars
-        </div>
+        <div className="task__title-and-priority__priority">{priority}</div>
       </div>
       <div className="task__status-date-and-label-icon">
         <p className="paragraph-secondary paragraph-secondary--bold task__status-date-and-label-icon__status">
@@ -22,11 +20,9 @@ const Task = ({ task }) => {
         </p>
         <p className="paragraph-secondary paragraph-secondary--bold task__status-date-and-label-icon__date">
           {due_date
-            ? formatDistance(
-                new Date(due_date),
-                new Date(),
-                { addSuffix: true }
-              )
+            ? formatDistance(new Date(due_date), new Date(), {
+                addSuffix: true,
+              })
             : ""}
         </p>
         <div className="task__status-date-and-label-icon__label-icon">
