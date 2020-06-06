8e44ca6b6b855250546e9abbb5eac4b17f829a3f
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-06-06 12:29:20 +0530

    Updated Compoenent Names

--- src/components/UnAuthApp.jsx
+++ src/components/UnAuthApp.jsx
@@ -3,17 +3,17 @@ import { Switch, Route } from "react-router-dom";
 import Loading from "./Loading/Loading";
 
 // lazy loading components
-const Login = lazy(() => import("./Login/Login"));
-const Register = lazy(() => import("./Register/Register"));
+const LoginPage = lazy(() => import("./Login/LoginPage"));
+const RegisterPage = lazy(() => import("./Register/RegisterPage"));
 const NotFound = lazy(() => import("./NotFoundPage/NotFoundPage"));
 
 const UnAuthApp = () => {
   return (
     <Suspense fallback={<Loading />}>
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
+        <Route exact path="/" component={LoginPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
         <Route component={NotFound} />
       </Switch>
     </Suspense>
