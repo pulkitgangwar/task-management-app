a5a6dcbf209060592458a684aa650c956630804d
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-06-06 12:26:17 +0530

    Updated NotFound Component

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NotFoundPage = ({ description, path, linkTitle }) => {
-  if (path) {
-    return (
-      <div className="notfound">
-        <h1 className="notfound__title heading-secondary">{description}</h1>
-        <div className="notfound__link btn btn--danger">
-          <Link to={path} className="notfound__link__link">{linkTitle}</Link>
-        </div>
+const NotFoundPage = () => {
+  return (
+    <div className="notfound">
+      <h1 className="notfound__title heading-secondary">404: Page Not Found</h1>
+      <div className="notfound__link btn btn--danger">
+        <Link to="/" className="notfound__link__link">
+          Go to Home
+        </Link>
       </div>
-    );
-  }
-  return <div>This is the Not Found Component</div>;
+    </div>
+  );
 };
 
 export default NotFoundPage;
