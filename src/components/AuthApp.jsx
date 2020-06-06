5f8a2fce46eba980fe3cb71c986361a475bb05b6
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-06-06 22:29:21 +0530

    Added stylesheets required by packages

--- src/components/AuthApp.jsx
+++ src/components/AuthApp.jsx
@@ -2,11 +2,15 @@ import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 import Loading from "./Loading/Loading";
 
+// Styles for react-datepicker and react-draft-wysiwyg
+import "react-datepicker/dist/react-datepicker.css";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
 // lazy loading components
 const Dashboard = lazy(() => import("./Dashboard/Dashboard"));
 const AddTask = lazy(() => import("./AddTask/AddTask"));
-const EditTask = lazy(() => import("./EditTask/EditTask"));
-const SingleTask = lazy(() => import("./SingleTask/SingleTask"));
+// const EditTask = lazy(() => import("./EditTask/EditTask"));
+// const SingleTask = lazy(() => import("./SingleTask/SingleTask"));
 const NotFoundPage = lazy(() => import("./NotFoundPage/NotFoundPage"));
 
 const AuthApp = () => {
@@ -15,8 +19,8 @@ const AuthApp = () => {
       <Switch>
         <Route exact path="/" component={Dashboard} />
         <Route exact path="/new" component={AddTask} />
-        <Route exact path="/edit/:id" component={EditTask} />
-        <Route exact path="/tasks/:id" component={SingleTask} />
+        {/* <Route exact path="/edit/:id" component={EditTask} /> 
+        <Route exact path="/tasks/:id" component={SingleTask} /> */}
         <Route component={NotFoundPage} />
       </Switch>
     </Suspense>
