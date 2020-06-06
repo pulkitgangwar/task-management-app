d7a03db6dd91c79f6d5736f96303c3c46f5ebd7c
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-06-06 12:22:25 +0530

    Added Route based Code Splitting

--- src/components/AuthApp.jsx
+++ src/components/AuthApp.jsx
@@ -1,31 +1,25 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
+import Loading from "./Loading/Loading";
 
-// importing Components
-import Dashboard from "./Dashboard/Dashboard";
-import AddTask from "./AddTask/AddTask";
-import EditTask from "./EditTask/EditTask";
-import SingleTask from "./SingleTask/SingleTask";
-import NotFoundPage from "./NotFoundPage/NotFoundPage";
+// lazy loading components
+const Dashboard = lazy(() => import("./Dashboard/Dashboard"));
+const AddTask = lazy(() => import("./AddTask/AddTask"));
+const EditTask = lazy(() => import("./EditTask/EditTask"));
+const SingleTask = lazy(() => import("./SingleTask/SingleTask"));
+const NotFoundPage = lazy(() => import("./NotFoundPage/NotFoundPage"));
 
 const AuthApp = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Dashboard} />
-      <Route exact path="/addtask" component={AddTask} />
-      <Route exact path="/edittask/:id" component={EditTask} />
-      <Route exact path="/tasks/:id" component={SingleTask} />
-      <Route
-        exact
-        render={() => (
-          <NotFoundPage
-            description="404 Page Not Found"
-            path="/"
-            linkTitle="Home"
-          />
-        )}
-      />
-    </Switch>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/new" component={AddTask} />
+        <Route exact path="/edit/:id" component={EditTask} />
+        <Route exact path="/tasks/:id" component={SingleTask} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Suspense>
   );
 };
 
--- src/components/UnAuthApp.jsx
+++ src/components/UnAuthApp.jsx
@@ -1,28 +1,22 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
+import Loading from "./Loading/Loading";
 
-// importing Components
-import Login from "./Login/Login";
-import Register from "./Register/Register";
-import NotFound from "./NotFoundPage/NotFoundPage";
+// lazy loading components
+const Login = lazy(() => import("./Login/Login"));
+const Register = lazy(() => import("./Register/Register"));
+const NotFound = lazy(() => import("./NotFoundPage/NotFoundPage"));
 
 const UnAuthApp = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Login} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route
-        exact
-        render={() => (
-          <NotFound
-            description="404 Page Not Found"
-            path="/"
-            linkTitle="Home"
-          />
-        )}
-      />
-    </Switch>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route component={NotFound} />
+      </Switch>
+    </Suspense>
   );
 };
 
