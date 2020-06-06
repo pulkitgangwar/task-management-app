42df46018ebbac8fccb1668d455104e83b34ba1b
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-06-06 22:31:38 +0530

    Improved Add Task Component

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,55 +1,13 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React from "react";
 
-// importing Components
 import AddTaskForm from "./AddTaskForm";
 import FloatingAnchor from "../FloatingCTA/FloatingAnchor";
-import Loading from "../Loading/Loading";
-
-// importing Task Client
-import { taskClient } from "../../apiClients/TaskClient";
 
 const AddTask = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const { push } = useHistory();
-  const addTask = async ({
-    title,
-    description,
-    status,
-    label,
-    priority,
-    due_date,
-  }) => {
-    try {
-      setLoading(true);
-      await taskClient.addTask({
-        title,
-        description,
-        status,
-        label,
-        priority,
-        due_date,
-      });
-
-      setLoading(false);
-      setError(null);
-      push("/");
-    } catch (err) {
-      setLoading(false);
-      setError(err);
-      console.log(err);
-    }
-  };
-
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
     <div className="addtask">
       <FloatingAnchor title="Go Back" path="/" />
-      <AddTaskForm addTask={addTask} error={error} />
+      <AddTaskForm />
     </div>
   );
 };
--- src/components/AddTask/AddTaskForm.jsx
+++ src/components/AddTask/AddTaskForm.jsx
@@ -1,36 +1,59 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-// import { Link } from "react-router-dom";
+import { EditorState, convertToRaw } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import { Link } from "react-router-dom";
+import { addTask } from "../../api/task";
 
-// importing stylesheet
-import "react-datepicker/dist/react-datepicker.css";
-
-const Form = ({ addTask, error }) => {
+const Form = () => {
   const [selectedDateAndTime, setSelectedDateAndTime] = useState(null);
-  const [isoTime, setIsoTime] = useState(null);
   const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const [description, setDescription] = useState(EditorState.createEmpty());
   const [status, setStatus] = useState("PENDING");
   const [label, setLabel] = useState("PERSONAL");
-  const [priority, setPriority] = useState("LOW");
+  const [priority, setPriority] = useState("NORMAL");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    addTask({
-      title,
-      description,
-      status,
-      label,
-      priority,
-      due_date: isoTime,
-    });
+    try {
+      setLoading(true);
+      await addTask({
+        title,
+        description: JSON.stringify(
+          convertToRaw(description.getCurrentContent())
+        ),
+        status,
+        label,
+        priority,
+        due_date: selectedDateAndTime
+          ? new Date(selectedDateAndTime).toISOString()
+          : null,
+      });
+      setLoading(false);
+      setError(null);
+      setSuccess(true);
+    } catch (error) {
+      setLoading(false);
+      setSuccess(false);
+      setError(error.response ? error.response.data.message : error.message);
+    }
   };
+
+  if (success)
+    return (
+      <p className="success">
+        Task Added Successfully ! <Link to="/">Go to All Tasks</Link>
+      </p>
+    );
 
   return (
     <form className="form" onSubmit={handleSubmit}>
       <h1 className="form__heading heading-primary">Add Task</h1>
 
-      <p className="error">{error && error}</p>
+      {error && <p className="error">{error}</p>}
 
       <div className="form__div form__div--title">
         <label htmlFor="form__input--title" className="form__label">
@@ -42,25 +65,12 @@ const Form = ({ addTask, error }) => {
           id="form__input--title"
           name="title"
           onChange={(e) => setTitle(e.target.value)}
+          maxLength={32}
+          required
           value={title}
           autoComplete="off"
         />
       </div>
-      <div className="form__div form__div--description">
-        <label htmlFor="form__input--description" className="form__label">
-          Description (optional)
-        </label>
-        <textarea
-          type="text"
-          className="form__input form__textarea"
-          id="form__input--description"
-          name="description"
-          onChange={(e) => setDescription(e.target.value)}
-          value={description}
-          autoComplete="off"
-        />
-      </div>
-
       <div className="form__container">
         <div className="form__div form__div--status">
           <label htmlFor="form__input--status" className="form__label">
@@ -70,6 +80,7 @@ const Form = ({ addTask, error }) => {
             id="form__input--status"
             value={status}
             onChange={(e) => setStatus(e.target.value)}
+            required
           >
             <option value="PENDING">Pending</option>
             <option value="IN_PROGRESS">In Progress</option>
@@ -84,6 +95,7 @@ const Form = ({ addTask, error }) => {
             id="form__input--label"
             value={label}
             onChange={(e) => setLabel(e.target.value)}
+            required
           >
             <option value="PERSONAL">Personal</option>
             <option value="WORK">Work</option>
@@ -94,7 +106,6 @@ const Form = ({ addTask, error }) => {
             <option value="OTHER">Other</option>
           </select>
         </div>
-
         <div className="form__div form__div--priority">
           <label htmlFor="form__input--priority" className="form__label">
             Priority
@@ -103,6 +114,7 @@ const Form = ({ addTask, error }) => {
             id="form__input--priority"
             value={priority}
             onChange={(e) => setPriority(e.target.value)}
+            required
           >
             <option value="LOW">Low</option>
             <option value="NORMAL">Normal</option>
@@ -110,29 +122,59 @@ const Form = ({ addTask, error }) => {
           </select>
         </div>
       </div>
-
+      <div className="form__div form__div--description">
+        <label htmlFor="form__input--description" className="form__label">
+          Description
+        </label>
+        <Editor
+          id="form__input--description"
+          name="description"
+          editorState={description}
+          toolbar={{
+            options: [
+              "inline",
+              "blockType",
+              "list",
+              "textAlign",
+              "link",
+              "embedded",
+              "emoji",
+              "history",
+            ],
+          }}
+          wrapperClassName="form__input form__textarea"
+          toolbarClassName="form__input form__toolbar"
+          editorClassName="form__input form__editor"
+          onEditorStateChange={(editorState) => setDescription(editorState)}
+        />
+      </div>
       <div className="form__div form__div--date-picker">
         <label htmlFor="form__input--date-picker" className="form__label">
-          Deadline for Task (optional)
+          Deadline{" "}
+          <button
+            className="btn btn--danger btn--danger--small"
+            type="button"
+            onClick={(e) => setSelectedDateAndTime(null)}
+          >
+            Clear
+          </button>
         </label>
-
         <DatePicker
-          autoComplete='off'
+          id="form__input--date-picker"
           selected={selectedDateAndTime}
           showTimeSelect
           dateFormat="Pp"
           onChange={(e) => {
-            setIsoTime(e.toISOString());
             setSelectedDateAndTime(e);
           }}
           className="form__date-picker"
           minDate={new Date()}
-          id="form__input--date-picker"
         />
       </div>
-
       <div className="form__btn--wrapper">
-        <button className="form__btn btn">Add Task</button>
+        <button disabled={loading} className="form__btn btn">
+          {loading ? "Processing..." : "Add Task"}
+        </button>
       </div>
     </form>
   );
