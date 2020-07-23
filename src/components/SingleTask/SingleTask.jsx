dfb3e4016d5808e626a308dcae736f8d09d7543f
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-07-23 16:32:28 +0530

    singletask component re-styled and made responsive

--- src/components/SingleTask/SingleTask.jsx
+++ src/components/SingleTask/SingleTask.jsx
@@ -78,55 +78,59 @@ const SingleTask = ({ match, history }) => {
 
   return (
     <div className="singletask">
-      <div className="singletask__wrapper">
-        <div className="singletask__cta-btns singletask__cta-btns--1">
-          <Link className="btn singletask__cta-btns__edit" to={`/edit/${id}`}>
-            Edit Task
-          </Link>
-          <button
-            className="btn btn--danger singletask__cta-btns__delete"
-            onClick={() => handleDelete(id)}
+      <div className="singletask__cta-btns singletask__cta-btns--1">
+        <Link className="btn singletask__cta-btns__edit" to={`/edit/${id}`}>
+          Edit Task
+        </Link>
+        <button
+          className="btn btn--danger singletask__cta-btns__delete"
+          onClick={() => handleDelete(id)}
+        >
+          Delete
+        </button>
+      </div>
+
+      <div className="singletask__task">
+        <div className="singletask__task__metadata">
+          <div className="singletask__task__metadata__status">
+            {status.toLowerCase()}
+          </div>
+          <div className="singletask__task__metadata__label">
+            {label.toLowerCase()}
+          </div>
+          <div
+            className={`singletask__task__metadata__priority singletask__task__metadata__priority--${priority}`}
           >
-            Delete
-          </button>
-        </div>
-        <div className="singletask__title">
-          <h3 className="heading-tertiary singletask__title__title ">
-            {title}
-          </h3>
-        </div>
-        <div className="singletask__description">
-          <p
-            className="paragraph-primary singletask__description__description"
-            dangerouslySetInnerHTML={{
-              __html: draftToHtml(JSON.parse(description)),
-            }}
-          ></p>
-        </div>
-        <div className="singletask__task-info">
-          <div className="singletask__task-info__div">
-            <p className="singletask__task-info__status--title">Status</p>
-            <h3 className="singletask__task-info__status--status singletask__value">
-              {status}
-            </h3>
-          </div>
-          <div className="singletask__task-info__div">
-            <p className="singletask__task-info__label--title">Label</p>
-            <h3 className="singletask__task-info__label--label singletask__value">
-              {label}
-            </h3>
-          </div>
-          <div className="singletask__task-info__div">
-            <p className="singletask__task-info__priority--title">Priority</p>
-            <h3 className="singletask__task-info__priority--priority singletask__value">
-              {priority}
-            </h3>
+            {priority.toLowerCase()}
           </div>
         </div>
-        <div className="singletask__time">
-          <div className="singletask__time__due_date">
-            <p className="singletask__time__due_date__placeholder">Due By</p>
-            <h3 className="singletask__time__due_date__due_date singletask__value">
+        <div className="singletask__task__title__and__desc">
+          <div className="singletask__task__title__and__desc__title">
+            <h1 className="singletask__task__title__and__desc__title--title">
+              {title}
+            </h1>
+          </div>
+          <div className="singletask__task__title__and__desc__description">
+            {!JSON.parse(description).blocks[0].text ? (
+              <p className="singletask__task__title__and__desc__description--placeholder">
+                Add a description
+              </p>
+            ) : (
+              <p
+                className="singletask__task__title__and__desc__description--description"
+                dangerouslySetInnerHTML={{
+                  __html: draftToHtml(JSON.parse(description)),
+                }}
+              ></p>
+            )}
+          </div>
+        </div>
+        <div className="singletask__task__date">
+          <div className="singletask__task__date__due-date">
+            <p className="singletask__task__date__due-date__placeholder">
+              Due By
+            </p>
+            <h3 className="singletask__task__date__due-date__value">
               {due_date
                 ? `${formatDistance(new Date(due_date), new Date(), {
                     addSuffix: true,
@@ -134,15 +138,19 @@ const SingleTask = ({ match, history }) => {
                 : "No Deadline"}
             </h3>
           </div>
-          <div className="singletask__time__created-at">
-            <p className="singletask__time__created-at__placeholder">Added</p>
-            <h3 className="singletask__time__created-at__created-at singletask__value">
+          <div className="singletask__task__date__created-at">
+            <p className="singletask__task__date__created-at__placeholder">
+              Added
+            </p>
+            <h3 className="singletask__task__date__created-at__value">
               {format(new Date(created_at), "dd/MM/yyyy HH:mm:ss a")}
             </h3>
           </div>
-          <div className="singletask__time__created-at">
-            <p className="singletask__time__created-at__placeholder">Updated</p>
-            <h3 className="singletask__time__created-at__created-at singletask__value">
+          <div className="singletask__task__date__updated-at">
+            <p className="singletask__task__date__updated-at__placeholder">
+              Updated
+            </p>
+            <h3 className="singletask__task__date__updated-at__value">
               {format(new Date(updated_at), "dd/MM/yyyy HH:mm:ss a")}
             </h3>
           </div>
